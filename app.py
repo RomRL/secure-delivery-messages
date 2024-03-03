8d8516b65e0ea0af512ebd84697dc78510355ea7
Rom <romharel98@gmail.com>
2024-03-03 13:46:18 +0200

    Chat UI

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ def initialize_session_state():
     if 'p' not in st.session_state or 'g' not in st.session_state:
         st.session_state.p, st.session_state.g = generate_prime(512), random.randint(2, 512)
 
-
     if 'private_key_alice' not in st.session_state or 'public_key_alice' not in st.session_state:
         st.session_state.public_key_alice, st.session_state.private_key_alice = generate_keypair(st.session_state.p,
                                                                                                  st.session_state.g)
@@ -29,7 +28,7 @@ def initialize_session_state():
 def main():
     st.set_page_config(page_title="Secure Delivery Messages", page_icon=":lock:")
     st.markdown('''
-    <h1 style='color: yellow; text-align: center;'>Secure Delivery Messages</h1>
+    <h1 style='color: yellow; text-align: center;'> 🔒 Secure Delivery Messages </h1>
     <div style='display: flex; justify-content: center;'>
         <div style='text-align: left;'>
             <p style='color: cyan; font-size: 20px; margin: 0;font-weight: bold;'>This is a secure chat application that demonstrates the following:<br></p>
@@ -91,21 +90,22 @@ def send_message(message, sender):
                                     private_key=st.session_state.private_key_bob if recipient == 'bob' else st.session_state.private_key_alice,
                                     public_key=st.session_state.public_key_bob if recipient == 'bob' else st.session_state.public_key_alice)
         with st.sidebar:
-                st.markdown('## Key Information : ')
-                st.write(f"### IV", unsafe_allow_html=True)
-                st.write(f"<span style='color:orange'>{st.session_state.iv.hex()}</span>",unsafe_allow_html=True)
-                st.write(f"### Original Key", unsafe_allow_html=True)
-                st.write(f"<span style='color:yellow'>{hex(key_to_transport)[2:]}</span>",unsafe_allow_html=True)
-                st.write(f"# {sender.upper()}")
-                st.write(f"### Message Key:")
-                st.write(f"- Encrypted Key (C1)",unsafe_allow_html=True)
-                st.write(f"<span style='color:cyan'>{hex(cipher_key[0])[2:]}{hex(decrypted_key)[2:]}</span>",unsafe_allow_html=True)
-                st.write(f"- Encrypted Key (C2)",unsafe_allow_html=True)
-                st.write(f"<span style='color:cyan'>{hex(cipher_key[1])[2:]}{hex(decrypted_key)[2:]}</span>",unsafe_allow_html=True)
-                st.write(f"# {recipient.upper()}")
-                st.write(f"{recipient} Decrypted Key: <span style='color:yellow'>{hex(decrypted_key)[2:]}</span>",
-                         unsafe_allow_html=True)
-
+            st.markdown('## Key Information : ')
+            st.write(f"### IV", unsafe_allow_html=True)
+            st.write(f"<span style='color:orange'>{st.session_state.iv.hex()}</span>", unsafe_allow_html=True)
+            st.write(f"### Original Key", unsafe_allow_html=True)
+            st.write(f"<span style='color:yellow'>{hex(key_to_transport)[2:]}</span>", unsafe_allow_html=True)
+            st.write(f"# {sender.upper()}")
+            st.write(f"### Message Key:")
+            st.write(f"- Encrypted Key (C1)", unsafe_allow_html=True)
+            st.write(f"<span style='color:cyan'>{hex(cipher_key[0])[2:]}{hex(decrypted_key)[2:]}</span>",
+                     unsafe_allow_html=True)
+            st.write(f"- Encrypted Key (C2)", unsafe_allow_html=True)
+            st.write(f"<span style='color:cyan'>{hex(cipher_key[1])[2:]}{hex(decrypted_key)[2:]}</span>",
+                     unsafe_allow_html=True)
+            st.write(f"# {recipient.upper()}")
+            st.write(f"{recipient} Decrypted Key: <span style='color:yellow'>{hex(decrypted_key)[2:]}</span>",
+                     unsafe_allow_html=True)
 
         serpent_cipher_encryption = SerpentCipherCBC(hexstring2bitstring(hexKey))
         encrypted_message = serpent_cipher_encryption.encrypt_cbc(message, st.session_state.iv)
@@ -121,10 +121,12 @@ def send_message(message, sender):
 def display_chat_logs():
     st.write("## Chat Log")
     for user in ['alice', 'bob']:
-        with st.chat_message(f"user") if user == 'alice' else st.chat_message(f"assistant"):
+        with st.chat_message(name="user", avatar='👱‍♀️') if user == 'alice' else st.chat_message(name='bob',
+                                                                                                 avatar='🙍‍♂️'):
             for msg in st.session_state.messages[user]:
                 if 'ALICE' in msg or 'BOB' in msg:
-                    st.markdown(f"<p style='color: yellow; font-size: 20px;font-weight: bold;'>{msg}</p>", unsafe_allow_html=True)
+                    str_markdown = f"<p style='color: yellow; font-size: 20px;font-weight: bold;'>{msg}</p>" if user == 'alice' else f"<p style='color: cyan; font-size: 20px;font-weight: bold;'>{msg}</p>"
+                    st.markdown(str_markdown, unsafe_allow_html=True)
                     continue
                 color = 'cyan' if 'Decrypted' in msg else 'red'
                 st.markdown(f"<p style='color: {color};'>{msg}</p>", unsafe_allow_html=True)
